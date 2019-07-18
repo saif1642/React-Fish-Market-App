ccca21c6d4ad62d61831ebb3e0a0017e26d64e12
shuttlesaifdev <saiful@shuttlebd.com>
2019-07-18 18:30:50 +0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
+class App extends React.Component{
+    state = {
+        fishes:{},
+        orders:{}
+    }
+    addFish = fish =>{
+        console.log(fish);
+        //1.Take a copy of existing state
+        const fishes = { ...this.state.fishes}
+        //2.add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        //3.Set the new fishes object to state
+        this.setState({
+            fishes:fishes
+        });
+    }
+    loadSampleFishes = () =>{
+        this.setState({ fishes : sampleFishes});
+    }
+    render(){
+        return (
+           <div className="catch-of-the-day">
+              <div className="menu">
+                  <Header tagline="saif is cool" age={500} cool={true}/>
+                  <ul className="fishes">
+                      {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                  </ul>
+              </div>
+              <Order />
+              <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+           </div>
+        );
+    }
+}
+
+export default App;
